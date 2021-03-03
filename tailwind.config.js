9de1bcbc037674612fcd55917c298bc6aacaadf3
tempakyousuke <tempakyousuke@gmail.com>
2021-03-04 03:45:00 +0900

    customize color

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      amber: colors.amber,
+      lime: colors.lime,
+      warmGray: colors.warmGray,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
