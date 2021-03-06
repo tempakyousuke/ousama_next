869c26a7ce7f9087896108f52f162ff7a9fccd8c
tempakyousuke <tempakyousuke@gmail.com>
2021-03-06 15:54:33 +0900

    set container

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ function Layout({ children }: { children: JSX.Element }): JSX.Element {
   return (
     <div>
       <Nav />
-      <div className="bg-amber-50">{children}</div>
+      <div className="container mx-auto">{children}</div>
     </div>
   );
 }
