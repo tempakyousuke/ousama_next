3c491fe2240d02354d6c773e958ff78edd884378
tempakyousuke <tempakyousuke@gmail.com>
2021-03-07 20:50:51 +0900

    create hiCard

---
+++ src/components/Card/hiCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type CardProps = {
+  title: string;
+};
+
+class HiCard extends React.Component<CardProps, {}> {
+  constructor(props: CardProps) {
+    super(props);
+  }
+
+  render(): JSX.Element {
+    return (
+      <div className="shadow-2xl rounded-xl">
+        <div className="p-3 text-xl text-white rounded-t-xl bg-amber-900">
+          {this.props.title}
+        </div>
+        <div className="p-6 bg-white rounded-b-xl">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default HiCard;
