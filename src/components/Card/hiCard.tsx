81932b0a236fc0d79f4b9680dcd7485f0834fc65
tempakyousuke <tempakyousuke@gmail.com>
2021-03-07 23:41:48 +0900

    add className

--- src/components/Card/hiCard.tsx
+++ src/components/Card/hiCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 type CardProps = {
   title: string;
+  className?: string;
 };
 
 class HiCard extends React.Component<CardProps, {}> {
@@ -11,11 +12,13 @@ class HiCard extends React.Component<CardProps, {}> {
 
   render(): JSX.Element {
     return (
-      <div className="shadow-2xl rounded-xl">
-        <div className="p-3 text-xl text-white rounded-t-xl bg-amber-900">
-          {this.props.title}
+      <div className={this.props.className}>
+        <div className="shadow-2xl rounded-xl">
+          <div className="p-3 text-xl text-white rounded-t-xl bg-amber-900">
+            {this.props.title}
+          </div>
+          <div className="p-6 bg-white rounded-b-xl">{this.props.children}</div>
         </div>
-        <div className="p-6 bg-white rounded-b-xl">{this.props.children}</div>
       </div>
     );
   }
