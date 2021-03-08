8435c60dbbb1040fbe23dd4d83bd3053e84f8ef4
tempakyousuke <tempakyousuke@gmail.com>
2021-03-09 00:53:40 +0900

    add toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import Layout from "../components/layout";
 import { AuthProvider } from "../context/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ToastContainer />
     </AuthProvider>
   );
 }
