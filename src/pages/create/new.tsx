f366f651bb4f9181e4138101811e5c7dc6932f31
tempakyousuke <tempakyousuke@gmail.com>
2021-03-26 19:03:53 +0900

    enable register board

--- src/pages/create/new.tsx
+++ src/pages/create/new.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Game } from "game/game";
+import { withRouter, NextRouter } from "next/router";
 import Position from "components/Game/position";
 import HiButton from "components/Button/hiButton";
+import { AuthContext } from "context/auth";
+import firebase, { firestore } from "utils/firebase";
+import { BLACK, WHITE } from "game/constant";
+interface WithRouterProps {
+  router: NextRouter;
+}
 
 type CreateNewState = {
   game: Game;
 };
-export default class CreateNew extends React.Component<{}, CreateNewState> {
-  constructor(props: {}) {
+class CreateNew extends React.Component<WithRouterProps, CreateNewState> {
+  static contextType = AuthContext;
+
+  constructor(props: WithRouterProps) {
     super(props);
     const game = new Game();
     game.tsumeInitialize();
@@ -15,6 +24,7 @@ export default class CreateNew extends React.Component<{}, CreateNewState> {
       game,
     };
     this.updateGame = this.updateGame.bind(this);
+    this.registerBoard = this.registerBoard.bind(this);
   }
 
   updateGame(value: Game): void {
@@ -23,14 +33,34 @@ export default class CreateNew extends React.Component<{}, CreateNewState> {
     }));
   }
 
+  async registerBoard() {
+    const uid = this.context.currentUser.uid;
+    const docRef = await firestore.collection("question_boards").add({
+      black_cap: this.state.game.cap[BLACK],
+      white_cap: this.state.game.cap[WHITE],
+      board: this.state.game.board,
+      uid,
+    });
+    const qRef = await firestore.collection("questions").add({
+      uid,
+      boardRef: docRef,
+      modified: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    this.props.router.push(`/create/edit/${qRef.id}`);
+  }
+
   render(): JSX.Element {
     return (
       <div className="mt-5">
         <Position game={this.state.game} updateGame={this.updateGame} />
         <div className="w-6/12 mx-auto mt-5">
-          <HiButton color="lime">登録</HiButton>
+          <HiButton color="lime" handleClick={this.registerBoard}>
+            登録
+          </HiButton>
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(CreateNew);
