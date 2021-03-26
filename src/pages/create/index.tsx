e4a204f6497a345bd98cdd23ef44ab09eb62af4a
tempakyousuke <tempakyousuke@gmail.com>
2021-03-26 20:53:08 +0900

    get data by swr

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+// import React from "react";
+import useSWR from "swr";
 import Link from "next/link";
 import { firestore } from "utils/firebase";
 import { AuthContext } from "context/auth";
+import { useContext } from "react";
 
-export default class CreateIndex extends React.Component {
-  render(): JSX.Element {
-    return <></>;
-  }
+export default function CreateList(): JSX.Element {
+  const { currentUser } = useContext(AuthContext);
+  const getQuestions = async () => {
+    const docs = await firestore
+      .collection("questions")
+      .where("uid", "==", currentUser.uid)
+      .orderBy("modified", "desc")
+      .get();
+    const boardTitles = [];
+    docs.forEach((doc) => {
+      boardTitles.push({
+        id: doc.id,
+        title: doc.data().title,
+        isApply: doc.data().isApply,
+      });
+    });
+    return boardTitles;
+  };
+
+  const boardTitles = useSWR("/question_board/", getQuestions);
+  console.log(boardTitles);
+
+  return <></>;
 }
