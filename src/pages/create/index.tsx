7932883cba690d61e1200a5c2db4f64f02c42098
tempakyousuke <tempakyousuke@gmail.com>
2021-03-26 19:49:56 +0900

    prepare question list page

---
+++ src/pages/create/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+import { firestore } from "utils/firebase";
+import { AuthContext } from "context/auth";
+
+export default class CreateIndex extends React.Component {
+  render(): JSX.Element {
+    return <></>;
+  }
+}
