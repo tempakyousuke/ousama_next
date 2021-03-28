f26f4e80c245d722d0f3d879a4f9b422c652de26
tempakyousuke <tempakyousuke@gmail.com>
2021-03-29 04:09:08 +0900

    fix path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { AuthProvider } from "../context/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
